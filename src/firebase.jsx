9cc295ad5cd2a5529930cd9a209c4bced7e909c4
Mus <138971833+Musxeto@users.noreply.github.com>
2024-03-23 14:07:55 +0500

    added firebase too hehehe

---
+++ src/firebase.jsx
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAuNAUayCyLQwcMvfLaCCE1oFwPhgWOlmo",
+  authDomain: "berserkgym-64268.firebaseapp.com",
+  projectId: "berserkgym-64268",
+  storageBucket: "berserkgym-64268.appspot.com",
+  messagingSenderId: "715156722697",
+  appId: "1:715156722697:web:82e661a5232228bcd3d4d8",
+};
+
+const app = initializeApp(firebaseConfig);
