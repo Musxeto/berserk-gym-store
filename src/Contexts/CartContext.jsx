4ba6190a4a99ffd6d062bc475c17b0136abbd609
Mus <138971833+Musxeto@users.noreply.github.com>
2024-03-30 14:56:05 +0500

    thak gaya aj

--- src/Contexts/CartContext.jsx
+++ src/Contexts/CartContext.jsx
@@ -1,13 +1,29 @@
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useState, useEffect } from "react";
 
 // Create a new context for the cart
 const CartContext = createContext();
 
 // Custom hook to access the cart context
 export const useCart = () => useContext(CartContext);
+
 // CartProvider component to wrap your application and provide the cart context
 export const CartProvider = ({ children }) => {
   const [cart, setCart] = useState([]);
+  const [total, setTotal] = useState(0);
+
+  useEffect(() => {
+    // Recalculate total whenever cart changes
+    calculateTotal();
+  }, [cart]);
+
+  // Function to calculate total
+  const calculateTotal = () => {
+    let newTotal = 0;
+    cart.forEach((item) => {
+      newTotal += item.productTotal;
+    });
+    setTotal(newTotal);
+  };
 
   // Function to add an item to the cart
   const addToCart = (item) => {
@@ -28,7 +44,7 @@ export const CartProvider = ({ children }) => {
 
   return (
     <CartContext.Provider
-      value={{ cart, addToCart, removeFromCart, clearCart }}
+      value={{ cart, total, addToCart, removeFromCart, clearCart }}
     >
       {children}
     </CartContext.Provider>
