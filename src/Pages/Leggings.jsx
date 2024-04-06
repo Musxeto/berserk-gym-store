c0110d92614800a8141b7dd87cc1b41801d89e9f
Mus <138971833+Musxeto@users.noreply.github.com>
2024-04-06 09:11:27 +0500

    lifting belts

--- src/Pages/Leggings.jsx
+++ src/Pages/Leggings.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import Layout from "../Components/Layout/Layout";
 
 const Leggings = () => {
-  return <div></div>;
+  const leggingsProducts = [
+    {
+      name: "Compression Leggings",
+      price: 24.99,
+      discount: 0,
+      image: "/compression_leggings.jpg",
+      hoverImage: "/compression_leggings_hover.jpg",
+      sizes: ["XS", "S", "M", "L", "XL"],
+    },
+  ];
+
+  return (
+    <div>
+      <Layout
+        pageTitle="Leggings Products"
+        pageDescription="Explore our collection of comfortable and stylish leggings."
+        products={leggingsProducts}
+      />
+    </div>
+  );
 };
 
 export default Leggings;
--- src/Pages/LiftingBelts.jsx
+++ src/Pages/LiftingBelts.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import Layout from "../Components/Layout/Layout";
 
 const LiftingBelts = () => {
-  return <div></div>;
+  // Sample data for lifting belts products
+  const liftingBeltsProducts = [
+    {
+      id: 1,
+      name: "Berserk Black Leather Lifting Belt",
+      price: 39.99,
+      discount: 0,
+      image: "/black_lifting_belt.jpg",
+      hoverImage: "/black_lifting_belt_hover.jpg",
+      sizes: ["S", "M", "L", "XL"],
+    },
+    {
+      id: 2,
+      name: "White Lifting Belt",
+      price: 29.99,
+      discount: 50,
+      image: "/white_lifting_belt.jpg",
+      hoverImage: "/white_lifting_belt_hover.jpg",
+      sizes: ["S", "M", "L", "XL"],
+    },
+  ];
+
+  return (
+    <div>
+      <Layout
+        pageTitle="Lifting Belts Products"
+        pageDescription="Browse our collection of high-quality lifting belts for your strength training needs."
+        products={liftingBeltsProducts}
+      />
+    </div>
+  );
 };
 
 export default LiftingBelts;
