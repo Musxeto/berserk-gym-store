bd6e8a699a4a266beab1872fc396f294afc30239
Ghulam Mustafa <138971833+Musxeto@users.noreply.github.com>
2025-01-31 17:50:45 +0500

    Update firebase.jsx

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -24,7 +24,12 @@ import {
 import { showFailureToast, showSuccessToast } from "./App";
 
 const firebaseConfig = {
-
+  apiKey: "AIzaSyAuNAUayCyLQwcMvfLaCCE1oFwPhgWOlmo",
+  authDomain: "berserkgym-64268.firebaseapp.com",
+  projectId: "berserkgym-64268",
+  storageBucket: "berserkgym-64268.appspot.com",
+  messagingSenderId: "715156722697",
+  appId: "1:715156722697:web:82e661a5232228bcd3d4d8"
 };
 
 const app = initializeApp(firebaseConfig);
